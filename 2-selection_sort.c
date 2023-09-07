3b50adf5c79d2647e13c982aa47efed571e6d40e
Vicky <63452@holbertonstudents.com>
2023-09-07 15:38:11 -0700

    betty fixed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,33 +2,33 @@
 
 /**
 * selection_sort - function that selects the smallest item
-and places that item at the top of the unsorted list.
+*and places that item at the top of the unsorted list.
 *@array: array to sort
 *@size: array size
 */
 
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min;
-    int temp;
+	size_t i, j, min;
+	int temp;
 
-    if (!array || size == 0)
+	if (!array || size == 0)
 	{
 		return;
 	}
-    for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-        min = i;
+		min = i;
 		for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min])
-            {
-                min = j;
-            }
-        }
-        temp = array[i];
-        array[i] = array[min];
-        array[min] = temp;
-        print_array(array, size);
-    }
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
+		print_array(array, size);
+	}
 }
