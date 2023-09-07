f2aac4efc5c6b5c554442a9a25018736efc8e335
Vicky <63452@holbertonstudents.com>
2023-09-07 16:00:02 -0700

    function_selection-sort_improvement

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -26,9 +26,12 @@ void selection_sort(int *array, size_t size)
 				min = j;
 			}
 		}
-		temp = array[i];
-		array[i] = array[min];
-		array[min] = temp;
-		print_array(array, size);
+		if (array[i] != array[min])
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
 	}
 }
